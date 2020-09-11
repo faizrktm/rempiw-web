09773b5e395eb042082ecc0a2cda7af7e0affca2
faizrktm <faizkort@gmail.com>
2020-09-11 08:49:51 +0700

    create hoc for translation handler server side

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,43 +1,11 @@
 import App from 'next/app';
-import LanguageProvider, { defaultLang } from 'utils/i18n';
-import cookies from 'utils/cookies';
-import Request from 'utils/request';
+import { appWithTranslation } from 'utils/i18n';
 
 // eslint-disable-next-line react/prop-types
-const MyApp = ({ Component, initialLanguage, pageProps }) => (
-  <LanguageProvider lang={initialLanguage}>
-    <Component {...pageProps} />
-  </LanguageProvider>
+const MyApp = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
 );
 
-MyApp.getInitialProps = async (appContext) => {
-  const appProps = await App.getInitialProps(appContext);
+MyApp.getInitialProps = async (appContext) => ({ ...await App.getInitialProps(appContext) });
 
-  /**
-   * Take a look at cookies lang, if does not same as lang on query
-   * redirect to lang site based on cookie.
-   * Basically only able to change language on click
-   * the options on header.
-   */
-  const { ctx } = appContext;
-  const lang = cookies.language.get(ctx);
-
-  const { res, req, pathname } = ctx;
-  const { baseUrl, queryStringify, language } = new Request(req);
-  if (lang !== language) {
-    if (res) {
-      const url = `${baseUrl}${pathname !== '/' ? pathname : ''}${lang ? `/${lang}` : ''}${queryStringify}`;
-      res.writeHead(302, {
-        Location: url,
-      });
-      res.end();
-    }
-  }
-
-  return ({
-    ...appProps,
-    initialLanguage: lang || defaultLang,
-  });
-};
-
-export default MyApp;
+export default appWithTranslation(MyApp);
--- src/utils/i18n/index.jsx
+++ src/utils/i18n/index.jsx
@@ -1,10 +1,13 @@
+/* eslint-disable react/prop-types */
 import { useContext, memo } from 'react';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import cookies from 'utils/cookies';
+import Request from 'utils/request';
+
 import translate from './translate';
 
-export const defaultLang = 'en';
+const defaultLang = 'en';
 
 const LanguageContext = React.createContext();
 
@@ -51,3 +54,42 @@ export function useLanguage() {
   const { t, language, changeLanguage } = useContext(LanguageContext);
   return { t, language, changeLanguage };
 }
+
+export const appWithTranslation = (Component) => class ChildClass extends React.Component {
+  static async getInitialProps(appContext) {
+    /**
+         * Take a look at cookies lang, if does not same as lang on query
+         * redirect to lang site based on cookie.
+         * Basically only able to change language on click
+         * the options on header.
+         */
+    const { ctx } = appContext;
+    const lang = cookies.language.get(ctx);
+
+    const { res, req, pathname } = ctx;
+    const { baseUrl, queryStringify, language } = new Request(req);
+    if (lang !== language) {
+      if (res) {
+        const url = `${baseUrl}${pathname !== '/' ? pathname : ''}${lang ? `/${lang}` : ''}${queryStringify}`;
+        res.writeHead(302, {
+          Location: url,
+        });
+        res.end();
+      }
+    }
+
+    return {
+      initialLanguage: lang || defaultLang,
+      ...await Component.getInitialProps(appContext),
+    };
+  }
+
+  render() {
+    const { initialLanguage, ...rest } = this.props;
+    return (
+      <LanguageProvider lang={initialLanguage}>
+        <Component {...rest} />
+      </LanguageProvider>
+    );
+  }
+};
