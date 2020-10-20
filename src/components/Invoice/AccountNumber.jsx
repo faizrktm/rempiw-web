80348a92cf671d9dfab1700677c427afebaa41ae
faizrktm <faizkort@gmail.com>
2020-10-20 15:14:55 +0700

    cabang bandung to bogor

--- src/components/Invoice/AccountNumber.jsx
+++ src/components/Invoice/AccountNumber.jsx
@@ -5,8 +5,8 @@ export default function AccountNumber() {
   return (
     <BorderedCard gap="medium">
       <Field label="Nomor Rekening" content="1234567890" contentClipboard="1234567890" />
-      <Field label="Nama Bank" content="BRI Cabang Bandung A.H Nasution" />
-      <Field label="Atas Nama" content="PT. Jasa Raharja (Persero) Cabang Jawa Barat" />
+      <Field label="Nama Bank" content="BRI Cabang Bogor" />
+      <Field label="Atas Nama" content="PT. Jasa Raharja (Persero) Cabang Bogor" />
     </BorderedCard>
   );
 }
